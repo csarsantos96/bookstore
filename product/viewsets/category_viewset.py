3ad00a5749fbe96bbf657ac57029782ed73a0f2e
Cesar Santos <csar.santos18@gmail.com>
2025-03-13 11:01:39 -0500

    feat: docker composer, docker images, config postgres on docker, bookstore_web on docker, Makefile.

--- product/viewsets/category_viewset.py
+++ product/viewsets/category_viewset.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import ModelViewSet
 from product.models import Category
 from product.serializers.category_serializer import CategorySerializer
 
+
 class CategoryViewSet(ModelViewSet):
-    queryset = Category.objects.all().order_by('id')
+    queryset = Category.objects.all().order_by("id")
     serializer_class = CategorySerializer
