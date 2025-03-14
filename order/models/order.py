332fbea72e355aaa705445af145f52db792fe965
Cesar Santos <csar.santos18@gmail.com>
2025-03-14 09:44:10 -0500

    feat: wip

--- order/models/order.py
+++ order/models/order.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
-
 from product.models import Product
-
 
 class Order(models.Model):
     product = models.ManyToManyField(Product, blank=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
